653a4221dec05e3f4bbdd565419e1df9f02eb0a5
RehaS <beratson@gmail.com>
2018-11-01 18:21:37 +0100

    koa server setup with kcors, body-parser, routing controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,2 +1,13 @@
-console.log("typescript configured");
-console.log("typsecript compile watch configured")
+import {useKoaServer} from 'routing-controllers';
+import {Server} from 'http';
+import * as Koa from 'koa';
+
+const app = new Koa();
+const server = new Server(app.callback());
+const port = process.env.PORT || 4000;
+
+useKoaServer(app, {
+  cors: true,
+})
+
+server.listen(port, x=> console.log("Koa Server alive!" + x))
