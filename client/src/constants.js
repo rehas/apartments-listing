9747d4777ef75068c06f3f50f2fd02b217c7f321
RehaS <beratson@gmail.com>
2018-11-04 17:03:39 +0100

    simple actions reducers structure, redux-dev-tools redux-thunk, jwt storage for browser memory, store, router wrappers

---
+++ client/src/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl =  'http://localhost:4000'
+
+export const localStorageJwtKey = 'currentUserJwt'
+
+/* process.env.REACT_APP_ENV === 'production' ? 'https://apartments-app-api.herokuapp.com' : */
