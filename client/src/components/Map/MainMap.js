4d6b467d629eccfe95083ff2b4f1b861a9a56eba
RehaS <beratson@gmail.com>
2018-11-08 17:21:41 +0100

    Map component wth googleMapsJS Api simple setup

---
+++ client/src/components/Map/MainMap.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 52.36,
+      lng: 4.90
+    },
+    zoom: 12
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBtwrZKzU9aPo9NVAuRL5thuWhWo0Cr1b8' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
