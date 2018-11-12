a67e1f65966f811dc4cb33bad9b9e24e95ed079b
RehaS <beratson@gmail.com>
2018-11-12 15:50:18 +0100

    map center moves with current list

--- client/src/components/Map/MainMap.js
+++ client/src/components/Map/MainMap.js
@@ -4,21 +4,48 @@ import { connect } from 'react-redux';
 import {Marker, markerStyle} from '../../styles/MarkerStyle'
 
 class MainMap extends Component {
+
+  averageLatLon = () =>{
+
+    const list = this.props.listedApartments
+
+    const averageLat= list && (list.page.reduce((agg, cur)=> {
+      
+      return agg = cur.lat + agg
+    }, 0) / list.page.length)
+
+
+    const averageLon= list && (list.page.reduce((agg, cur)=> {
+      return agg = cur.lon + agg
+    }, 0) / list.page.length)
+
+    return {
+      lat: averageLat,
+      lng: averageLon
+    }
+  }
+
   static defaultProps = {
     center: {
       lat: 52.36,
       lng: 4.90
     },
-    zoom: 12
+    zoom: 10
   };
 
   render() {
+    
+    let ctr = this.props.center
+    if(this.averageLatLon().lat && this.averageLatLon().lng){
+      ctr = this.averageLatLon()
+    }
     return (
       // Important! Always set the container height explicitly
       <div style={{ height: '95vh', width: '100%' }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: 'AIzaSyBtwrZKzU9aPo9NVAuRL5thuWhWo0Cr1b8' }}
           defaultCenter={this.props.center}
+          center={ctr}
           defaultZoom={this.props.zoom}
         >
         {this.props.listedApartments && 
